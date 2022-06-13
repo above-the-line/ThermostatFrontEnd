6628d3ad992db143be3d1938e0ac480096ac6481
Avigdor Michael <avigdormichael@192-168-1-109.tpgi.com.au>
2022-06-13 16:37:54 +1000

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        <h1 className="text-4xl font-bold underline">
+          Hello world!
+        </h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
